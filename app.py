80e5167cccc11e1d2372937dbf6c1b3715ee21cd
zTheo47 <stanley.alves@gmail.com>
2026-05-03 15:39:05 -0300

    fix: remove dev server, use gunicorn only

--- app.py
+++ app.py
@@ -37,9 +37,3 @@ def calculate():
 @app.route("/ping", methods=["GET"])
 def ping():
     return {"message": "API online"}
-
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
