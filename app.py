9cf05e14a085191ae44c394dc7e1918b83728955
zTheo47 <stanley.alves@gmail.com>
2026-05-03 15:17:48 -0300

    fix: porta render

--- app.py
+++ app.py
@@ -38,4 +38,8 @@ def calculate():
 def ping():
     return {"message": "API online"}
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
